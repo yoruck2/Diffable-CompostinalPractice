21a8b716025f4000252666125a0ea0c54d527ffb
yoruck2 <yoruck2@gmail.com>
2024-07-20 20:12:57 +0900

    ✨ TalkListView 구현

--- Diffable&CompostinalPractice/TalkListView.swift
+++ Diffable&CompostinalPractice/TalkListView.swift
@@ -12,13 +12,27 @@ import Then
 
 final class TalkListView: BaseView {
     
-    let talkListCollectionView = UICollectionView()
-    
-    let searchBar = UISearchBar().then {
-        $0.placeholder = "채팅방 검색"
+    lazy var chatListcollectionView = UICollectionView(frame: .zero,
+                                                       collectionViewLayout: createLayout())
+    func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .absolute(100))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(100))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
     
+    override func configureHierarchy() {
+        addSubview(chatListcollectionView)
+    }
     
-    
-    
+    override func configureLayout() {
+        chatListcollectionView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
 }
