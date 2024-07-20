4b85cc76da0e781b1569a6a048dbff1b660be958
yoruck2 <yoruck2@gmail.com>
2024-07-20 20:12:03 +0900

    ✨ ChatListcollectionView 및 검색 기능 구현 DiffableDataSource, Compostional Layout 적용

--- Diffable&CompostinalPractice/TalkListViewController.swift
+++ Diffable&CompostinalPractice/TalkListViewController.swift
@@ -7,29 +7,84 @@
 
 import UIKit
 
-class TalkListViewController: BaseViewController<TalkListView> {
-    
-    lazy var ChatListcollectionView = UICollectionView(frame: .zero, 
-                                                       collectionViewLayout: createLayout())
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationItem.titleView = rootView.searchBar
-    }
-    func createLayout() -> UICollectionViewLayout {
-        
-        var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-        configuration.showsSeparators = true
-        configuration.headerMode = .supplementary
-        
-        let layout = UICollectionViewCompositionalLayout
-            .list(using: configuration)
-        return layout
-    }
-    
+enum Section: CaseIterable {
+    case chatRoom
 }
 
-
-extension TalkListViewController: UISearchBarDelegate {
+class TalkListViewController: BaseViewController<TalkListView> {
     
+    var dataSource: UICollectionViewDiffableDataSource<Section, ChatRoom>!
+    var registeration: UICollectionView.CellRegistration<TalkListCollectionViewCell, ChatRoom>!
+    var mockData = MockChat().mockChatList
+    
+    lazy var searchedRoomList: [ChatRoom] = mockData {
+        didSet {
+            updateSnaphot()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureDataSource()
+        updateSnaphot()
+        configureSearchBar()
+    }
+    
+    override func configureView() {
+        navigationItem.title = "COCOA TALK"
+        
+    }
+}
+
+extension TalkListViewController {
+    
+    func configureDataSource() {
+        registeration = UICollectionView.CellRegistration { cell, indexPath, itemIdentifier in
+            cell.talkRoomNameLabel.text = itemIdentifier.chatroomName
+            cell.profileImageView.image = UIImage(named: itemIdentifier.chatroomImage)
+            cell.recentTalkLabel.text = itemIdentifier.chatList.last?.message
+            cell.dateLabel.text = itemIdentifier.chatList.last?.date.formattedDotDate
+        }
+        
+        self.dataSource = UICollectionViewDiffableDataSource(collectionView: self.rootView.chatListcollectionView,
+                                                             cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: self.registeration,
+                                                                    for: indexPath,
+                                                                    item: itemIdentifier)
+            return cell
+        })
+    }
+    func updateSnaphot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, ChatRoom>()
+        snapshot.appendSections([.chatRoom])
+        snapshot.appendItems(searchedRoomList, toSection: .chatRoom)
+        dataSource.apply(snapshot)
+    }
+}
+extension TalkListViewController: UISearchResultsUpdating {
+    
+    func configureSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+        searchController.isActive = true
+        searchController.searchBar.placeholder = "친구 이름을 검색해보세요"
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let text = searchController.searchBar.text?.lowercased() else {
+            searchedRoomList = mockData
+            return
+        }
+        
+        if text == "" {
+            searchedRoomList = mockData
+            
+        } else {
+            searchedRoomList = mockData.filter {
+                $0.chatroomName.lowercased().contains(text)
+            }
+        }
+    }
 }
